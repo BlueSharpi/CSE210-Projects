7e57ffd19da8f0db595be87993c6300d1bc3e2e3
JacobWright <jacobb.wright98@gmail.com>
2023-03-09 19:43:12 -0500

    Entry code optimizations

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,14 +4,21 @@ public class Entry
     public string _prompt;
     public string _response;
 
+    public Entry(string date, string prompt, string response)
+    {
+        _date = date;
+        _prompt = prompt;
+        _response = response;
+    }
+
     public void DisplayEntry()
     {
-        string result = "\n" + _date + "\n" + _prompt + "\n" + _response;
+        string result = "\n" + _date + "\n  " + _prompt + "\n  " + _response;
         Console.WriteLine($"{result}");
     }
     public string FormatEntryForCSV()
     {
-        string result = string.Format("{0} | {1} | {2}", _date, _prompt, _response);
+        string result = string.Format("{0}|{1}|{2}", _date, _prompt, _response);
         return result;
     }
 }
