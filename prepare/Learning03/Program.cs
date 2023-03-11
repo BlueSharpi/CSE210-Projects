fa2248a02a4cb652037899e42a261bd1088e0231
JacobWright <jacobb.wright98@gmail.com>
2023-03-11 12:13:42 -0500

    fractions!

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f = new Fraction(1,3);
+        Console.Clear();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine($"{f.GetDecimalValue()}");
+
     }
 }
